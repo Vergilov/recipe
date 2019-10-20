c2c9f8416dd5cfdf5e5371c0f47554261da241e2
Vergilov <biggohunter@gmail.com>
2019-10-20 23:57:46 +0200

    Complete Assignment

---
+++ src/main/java/com/vergilov/recipe/domain/Ingredient.java
@@ -0,0 +1,42 @@
+package com.vergilov.recipe.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String description;
+    private BigDecimal amount;
+
+    private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
