6d201f2c401206fa11bf16a992d015bb8eecd36e
Vergilov <biggohunter@gmail.com>
2019-10-22 11:34:20 +0200

    Update UnitOfMeasure and Category Repository

--- src/main/java/com/vergilov/recipe/controllers/IndexController.java
+++ src/main/java/com/vergilov/recipe/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package com.vergilov.recipe.controllers;
 
+import com.vergilov.recipe.domain.Category;
+import com.vergilov.recipe.domain.UnitOfMeasure;
+import com.vergilov.recipe.repositories.CategoryRepository;
+import com.vergilov.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional=categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat ID is: "+categoryOptional.get().getId());
+        System.out.println("UOM ID is: "+unitOfMeasureOptional.get().getId());
+
         System.out.println("SOME MESSAGE TO SAY MONs!");
         return "index";
     }
